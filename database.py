5d813aa13332df576a508d3431eca5952df0e19e
laurent <chanadet30@gmail.com>
2026-05-04 14:46:07 +0200

    fix Base import

--- database.py
+++ database.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./database.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./adminflow.db"
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# 🔥 C'EST ÇA QUI MANQUE
+Base = declarative_base()
